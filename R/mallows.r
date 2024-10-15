78d8d8eb1718cf7b4f70cbe38cf7141a1d202542
afr063426 <joshua@LAPTOP-0RHC2H35>
2024-10-14 20:06:28 -0600

    feauture: add mallows split

--- R/mallows.r
+++ R/mallows.r
@@ -11,7 +11,7 @@ histogramas_ejemplo <- iris  %>%
     reframe(Petal.Width = sym.histogram(x = Petal.Width, breaks = pretty(Petal.Width, 5) ))
 prueba <- histogramas_ejemplo$Petal.Width[1]
 
-plot(histogramas_ejemplo[1,2])
+
 #' Uniform intervals, it has as objective to uniform the interval 
 #' @param x it is a symbolic histogram
 #' @param quantile uniform length of the quantiles
@@ -59,6 +59,47 @@ uniform_histogram <- function(x, length_quantiles = 0.1, props = NULL){
 }
 
 
+mean.symbolic_histogram <- function(x){
+    breaks <- x$breaks 
+
+    breaks_mean <- Reduce(`+`, breaks)/length(breaks[[1]])
+    props <- x$props[1]
+    out <- list(
+        breaks = breaks_mean, 
+        props = props
+    )
+    vctrs::new_vctr(list(out), class = "symbolic_histogram")   
+}
+
+mse_histogram <- function(y, pred){
+    if(length(y) > 1){
+        breaks <- y$breaks
+        centers <- lapply(breaks, function(x) na.omit((x+lag(x))/2))
+        centers[[1]]
+        ranges <- lapply(breaks, function(x) na.omit(x-lag(x)))
+        
+        breaks_mean <- pred$breaks
+
+        centers_mean <- (breaks_mean + lag(breaks_mean))/2
+        ranges_mean <- (breaks_mean - lag(breaks_mean))
+        centers_mean <- centers_mean[!is.na(centers_mean)]
+        ranges_mean <- ranges_mean[!is.na(ranges_mean)]
+
+        props <- y$props
+        
+        sum_centers <- sapply(centers, function(x) sum(props[[1]][1]*(x - centers_mean)^2))
+        sum_ranges <- 1/3*sapply(ranges, function(x) sum(props[[1]][1]*(x - ranges_mean)^2))
+        return(sum_centers + sum_ranges)
+    }else{
+        return(0)
+    }
+    
+}
+
+histogramas_ejemplo <- histogramas_ejemplo %>%
+    group_by(Species) %>%
+    mutate(histograma = uniform_histogram(Petal.Width)) 
+histogramas_ejemplo$prueba <- c(1,2,3)
 #Necesito ver como vectorizo esto
 
 #uniform_histogram <- Vectorize(uniform_histogram, vectorize.args = "x")
@@ -74,9 +115,8 @@ mallows_initialization <- function(y, offset, parms, wt){
 
     if (!missing(parms) && length(parms) > 0) warning("parameter argument ignored")
 
-
     sfun <- function(yval, dev, wt, ylevel, digits ) {
-        paste(" mean=", format(signif(yval, digits)),
+        paste(" hist=", format(signif(yval, digits)),
         ", RMSE=" , format(signif(dev/wt, digits)),
         sep = '')
     }
@@ -86,53 +126,94 @@ mallows_initialization <- function(y, offset, parms, wt){
     
 }
 
+
 #' @param y value of the observation
 #' @param wt weight of the observation 
 #' @param parms parameteres given by the user
 mallows_evaluation <- function(y, wt, parms){
+    y <- histogramas_ejemplo$histograma
     
     #Diferencias con medida dada por brito
+    breaks <- y$breaks 
 
-    # wmean <- sum(y*wt)/sum(wt)
+    breaks_mean <- Reduce(`+`, breaks)/length(breaks[[1]])
+    centers <- lapply(breaks, function(x) na.omit((x+lag(x))/2))
+    ranges <- lapply(breaks, function(x) na.omit(x-lag(x)))
+
+    centers_mean <- (breaks_mean + lag(breaks_mean))/2
+    ranges_mean <- (breaks_mean - lag(breaks_mean))
+    centers_mean <- centers_mean[!is.na(centers_mean)]
+    ranges_mean <- ranges_mean[!is.na(ranges_mean)]
+
+    props <- y$props
+    
+    sum_centers <- sapply(centers, function(x) sum(props[[1]][1]*(x - centers_mean)^2))*wt
+    sum_ranges <- 1/3*sapply(ranges, function(x) sum(props[[1]][1]*(x - ranges_mean)^2))*wt
+    deviance <- (sum(sum_centers) + sum(sum_ranges))
+
+
+    # izquierdo <- mean_centers - mean_ranges
+    # derecho <-  mean_centers + mean_ranges
+    out <- list(
+        breaks = breaks_mean, 
+        props = props[[1]]
+    )
+
+    
     # rss <- sum(wt*(y-wmean)^2)
-    #list(label = wmean, deviance = rss)
+    list(label = vctrs::new_vctr(list(out), class = "symbolic_histogram"), deviance = deviance)
 }
 
 
 
-mallows_split <- function(y, wt, x, parms, continuous){
+
+mallows_split <- function(y, wt = NULL, x, parms = NULL, continuous = TRUE){
     # Center y
-    n <- length(y)
-    y <- y- sum(y*wt)/sum(wt)
+    
     if (continuous) {
-    # continuous x variable
-    temp <- cumsum(y*wt)[-n]
-    left.wt <- cumsum(wt)[-n]
-    right.wt <- sum(wt) - left.wt
-    lmean <- temp/left.wt
-    rmean <- -temp/right.wt
-    goodness <- (left.wt*lmean^2 + right.wt*rmean^2)/sum(wt*y^2)
-    list(goodness = goodness, direction = sign(lmean))
-    } else {
-    # Categorical X variable
-    ux <- sort(unique(x))
-    wtsum <- tapply(wt, x, sum)
-    ysum <- tapply(y*wt, x, sum)
-    means <- ysum/wtsum
-    # For anova splits, we can order the categories by their means
-    # then use the same code as for a non-categorical
-    ord <- order(means)
-    n <- length(ord)
-    temp <- cumsum(ysum[ord])[-n]
-    left.wt <- cumsum(wtsum[ord])[-n]
-    right.wt <- sum(wt) - left.wt
-    lmean <- temp/left.wt
-    rmean <- -temp/right.wt
-    list(goodness= (left.wt*lmean^2 + right.wt*rmean^2)/sum(wt*y^2),
-    direction = ux[ord])
+    mse <- Inf
+    x <- histogramas_ejemplo$prueba
+        for(i in 1:(length(x) - 1)){
+            
+            left <- which(x[i] >= x)
+
+            right <- which(x[i] < x)
+            direction <- ifelse(x[i] >= x, -1, 1)
+
+            left_mean <- mean(y[left])
+            right_mean <- mean(y[right])
+            left_mse <- mse_histogram(y[left], left_mean)
+            right_mse <- mse_histogram(y[right], right_mean)
+
+            mse_aux <- sum((sum(x <= x[i])*left_mse + sum(x > x[i])*right_mse))/length(x)
+            print(mse_aux)
+            if(mse > mse_aux){
+                df_aux <- data.frame(mse = c(left_mse, right_mse), indice = c(left, right)) %>%
+                    arrange(indice)
+                mse <- mse_aux
+                goodness <- -df_aux$mse
+                split <- direction
+            }
+
+        }
+    
+    
     }
+    list(goodness = goodness, direction = split)
 }
 
+
+
+# ------------------------------------------------
+# ---------------- PRUEBA -----------------------
+
+ulist <- list(eval = mallows_evaluation, split = mallows_split, init = mallows_initialization)
+histogramas_ejemplo_aux <- histogramas_ejemplo %>%
+    ungroup %>%
+    mutate(texto = "a", collapse = ":") %>% dplyr::select(texto, prueba)
+
+prueba <- rpart(texto ~ prueba , method = ulist, data = histogramas_ejemplo_aux )
+formula <- histograma ~ prueba
 
 # Ejemplo
 # > ulist <- list(eval = mallows_evaluation, split = mallows_split, init = mallows_initialization)
@@ -142,3 +223,5 @@ mallows_split <- function(y, wt, x, parms, continuous){
 # > fit2 <- rpart(murder ~ population + illiteracy + income + life.exp +
 # hs.grad + frost + region, data = mystate,
 # method = 'anova', minsplit = 10, xval = 0)
+
+
